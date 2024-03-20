2a882b514d97e6239139d5139fdc1aafa4e150d8
Andrea Auñón Antúnez <andrea.aunon@babelgroup.com>
2024-03-20 11:05:11 +0100

    base

---
+++ src/main/java/dataaccess/com/webservicesserver/WebservicesserverApplication.java
@@ -0,0 +1,13 @@
+package dataaccess.com.webservicesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dataaccess/com/webservicesserver/WebservicesserverApplicationTests.java
@@ -0,0 +1,13 @@
+package dataaccess.com.webservicesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
